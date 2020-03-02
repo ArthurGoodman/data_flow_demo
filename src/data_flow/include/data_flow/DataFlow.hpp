4605eec5fc09c76cd3061044102bc4bd1ae70e22
ArthurGoodman <arthur.goodman3@gmail.com>
2020-03-02 16:24:11 +0200

    First impl

--- src/data_flow/include/data_flow/DataFlow.hpp
+++ src/data_flow/include/data_flow/DataFlow.hpp
@@ -1,22 +1,81 @@
 #pragma once
 
+#include <cassert>
 #include <functional>
+#include <iostream>
+#include <map>
 #include <memory>
+#include <type_traits>
+#include <typeindex>
+#include <utility>
+#include <vector>
 
 namespace DataFlowDemo {
 
 class CDataFlow
 {
-public: // methods
-    template <class T>
-    void registerDataHandler(const std::function<void(const T &)> & /*handler*/)
+private: // types
+    struct IHandler
     {
+        virtual ~IHandler() = default;
+    };
+
+    template <class T>
+    using HandlerCallback = std::function<void(const T &)>;
+
+    template <class T>
+    struct Handler final
+        : HandlerCallback<T>
+        , IHandler
+    {
+        explicit Handler(HandlerCallback<T> &&func)
+            : HandlerCallback<T>{std::move(func)}
+        {
+        }
+    };
+
+private: // methods
+    template <class T>
+    static std::type_index TypeIndex()
+    {
+        return typeid(std::decay_t<T>);
+    }
+
+public: // methods
+    template <class T, class F>
+    void registerDataHandler(F &&func)
+    {
+        m_handlers[TypeIndex<T>()].emplace_back(new Handler<T>{std::forward<F>(func)});
     }
 
     template <class T>
-    void send(const T & /*data*/) const
+    void send(const T &data) const
     {
+        const auto &it = m_handlers.find(TypeIndex<T>());
+
+        if (it == m_handlers.end())
+        {
+            std::cerr << "error: handler not found for type '" << TypeIndex<T>().name() << "'"
+                      << std::endl;
+            return;
+        }
+
+        for (const auto &ptr : it->second)
+        {
+            auto handler = dynamic_cast<Handler<T> *>(ptr.get());
+
+            if (!handler)
+            {
+                assert(!"should not happen");
+                return;
+            }
+
+            (*handler)(data);
+        }
     }
+
+private: // fields
+    std::map<std::type_index, std::vector<std::unique_ptr<IHandler>>> m_handlers;
 };
 
 using DataFlowPtr = std::shared_ptr<CDataFlow>;
